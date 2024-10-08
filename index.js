c40fdb486453b1a42e4f93f87020959a005737fb
Saja-Albinali <sajaalbinali176@gmail.com>
2024-10-08 10:26:08 +0300

    arrays1

---
+++ index.js
@@ -0,0 +1,17 @@
+const groceries = [
+  "apples",
+  "tomatoes",
+  "chocolate",
+  "strawberries",
+  "oranges",
+  "bananas",
+];
+console.log(groceries[1]);
+console.log(groceries.length);
+const lastGroceriers = console.log(groceries[groceries.length - 1]);
+groceries.pop();
+const addNew = ("raspberries", "bluberries");
+console.log(groceries.push`${addNew}`);
+console.log[(groceries[0], groceries[1], groceries[2])];
+console.log(groceries[groceries.splice(0, 3)]);
+const addNewElement = console.log[groceries.shift];
